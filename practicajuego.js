2405e51ab9559446eaea97c5781bff32d59588a8
paissly <98744062+paissly@users.noreply.github.com>
2024-02-16 13:29:41 +0100

    Add files via upload

--- practicajuego.js
+++ practicajuego.js
@@ -35,16 +35,40 @@ function create() {
     this.add.image(400, 300, 'sky');
    
     platforms= this.physics.add.staticGroup();
-    platforms.create(400,568,'ground').setScale(2);
+    platforms.create(400,568,'ground').setScale(2).refreshBody();
     platforms.create(600,400,'ground');
     platforms.create(50,250,'ground');
     platforms.create(750,220,'ground');
+
+
+    player = this.physics.add.sprite(100,450,'dude');
+    player.setCollideWorldBounds(true); //el muñeco no cae al vacio, respeta los límites del juego
+    player.setBounce(0.2); //el muñeco rebota al caer
+
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude',{start: 0, end: 3}),
+        frameRete: 10, //se ejecuta a velocidad de 10 fotogramas por segundo
+        repeat: -1
+    });
 }
+    this.anims.create({
+        key: 'turn',
+        frames: [ {key:'dude',frame:4} ],
+        frameRete: 20, //se ejecuta a velocidad de 20 fotogramas por segundo
+    });
+
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude',{start: 5, end: 8}),
+        frameRete: 10, //se ejecuta a velocidad de 10 fotogramas por segundo
+        repeat: -1
+    });
 
 
 function update() {
 
 
 }
-
-
