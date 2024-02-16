f63ea0aa03d15e66c065e11ca4a612fadd8e62c8
paissly <98744062+paissly@users.noreply.github.com>
2024-02-16 13:26:21 +0100

    Add files via upload

--- practicajuego.js
+++ practicajuego.js
@@ -6,7 +6,7 @@ type: Phaser.AUTO,
 width: 800,
 height: 600,
 scene: {
-    perload: preload,
+    preload: preload,
     create: create,
     update: update
 }
@@ -15,9 +15,17 @@ var game = new Phaser.Game(config);
 
 
 function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude','assets/dude.png',{frameWidth:32, frameHeight:48});
 }
 function create() {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'start');
 }
 function update(){
-   
+
+
 }
