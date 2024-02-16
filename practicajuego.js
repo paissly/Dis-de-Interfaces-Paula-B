0d76645ae66bef3d0b51ed44dacb402aec29e3a9
paissly <98744062+paissly@users.noreply.github.com>
2024-02-16 13:27:20 +0100

    Add files via upload

--- practicajuego.js
+++ practicajuego.js
@@ -1,15 +1,23 @@
-const Phaser = require("phaser");
+// Elimina esta línea:
+// const Phaser = require("phaser");
 
 
 var config = {
-type: Phaser.AUTO,
-width: 800,
-height: 600,
-scene: {
-    preload: preload,
-    create: create,
-    update: update
-}
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300}
+            // debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 var game = new Phaser.Game(config);
 
@@ -19,13 +27,24 @@ function preload() {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude','assets/dude.png',{frameWidth:32, frameHeight:48});
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
+
+
 function create() {
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'start');
+   
+    platforms= this.physics.add.staticGroup();
+    platforms.create(400,568,'ground').setScale(2);
+    platforms.create(600,400,'ground');
+    platforms.create(50,250,'ground');
+    platforms.create(750,220,'ground');
 }
-function update(){
+
+
+function update() {
 
 
 }
+
+
